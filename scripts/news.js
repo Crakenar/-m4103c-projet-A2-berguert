cbadd3ba86da5dc4886b80bf7d1b291dcbd32aa5
Crakenar <teothou@hotmail.fr>
2020-03-23 00:47:20 +0100

    sdf

--- scripts/news.js
+++ scripts/news.js
@@ -152,7 +152,7 @@ function supprimer_nouvelle(elt) {
 $("#zone_saisie").autocomplete( "option", "source", JSON.parse(recherches).Result)
 
 // Recherche Quand touche entrée
-$("#zone_saisie").keypress(function(event){
+$("#form1").keypress(function(event){
 	let keycode = (event.keycode ? event.keycode : event.which);
 	if(keycode == '13'){
 		rechercher_nouvelles();
